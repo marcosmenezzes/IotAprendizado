95b693f615a5d5690eb60a8340645b1617e8f0e6
Marcos Menezzes <marcosmenezzez@gmail.com>
2025-05-15 21:42:06 -0300

    Add files via upload

--- Mqtt_conect_CTDS12.ino
+++ Mqtt_conect_CTDS12.ino
@@ -6,7 +6,7 @@ char ssid[] = "Rodolfo_Roteador";     // Nome da rede Wi-Fi
 char pass[] = "rodolfoinstrutor";     // Senha da rede Wi-Fi
 
 // Configuração do servidor MQTT (Broker)
-const char* IP_do_servidor = "10.140.4.42"; // IP do broker MQTT
+const char* IP_do_servidor = "10.140.4.49"; // IP do broker MQTT
 int Porta_do_servidor = 1883;               // Porta padrão do protocolo MQTT
 
 // Objetos necessários para a comunicação Wi-Fi e MQTT
@@ -39,7 +39,7 @@ void setup()
   }
 
   // Inscreve-se no tópico "Led1_rodolfo" para receber comandos de controle do LED
-  MQTT.subscribe("Led1_rodolfo");
+  MQTT.subscribe("LUZ");
 
   Serial.println("Mqtt conectado");      // Mensagem indicando conexão bem-sucedida
 }
@@ -48,7 +48,6 @@ void loop()
 {
   delay(3000);                            // Aguarda 3 segundos entre iterações
   MQTT.loop();                            // Mantém a conexão com o broker ativa (keep-alive)
-  MQTT.publish("topico_teste", "OLA");   // Publica a mensagem "OLA" no tópico "topico_teste"
 }
 
 // Função chamada sempre que uma mensagem é recebida em um dos tópicos inscritos
@@ -62,13 +61,16 @@ void resposta(char* topico, byte* conteudo, unsigned int comprimento)
     minha_string = minha_string + (char)conteudo[i];
   }
 
+Serial.println(minha_string); 
   // Verifica o conteúdo da mensagem e aciona o LED de acordo
-  if(minha_string == "on")                // Se a mensagem for "on"
+  if(minha_string == "diabo!")                // Se a mensagem for "on"
   {
-    digitalWrite(2, 0);                   // Liga o LED (nível baixo)
+    digitalWrite(2, 0); 
+     MQTT.publish("escolha", "Diaboooo");              
   }
-  else if(minha_string == "off")          // Se a mensagem for "off"
+  else if(minha_string == "jesus!!")          // Se a mensagem for "off"
   {
-    digitalWrite(2, 1);                   // Desliga o LED (nível alto)
+    digitalWrite(2, 1);   
+     MQTT.publish("escolha","Jesus");               
   }
 }
